48e038dfd2c7f60690dfbee72efc9198b51726b6
thorxenon <pedrolucasrangel1@hotmail.com>
2025-03-09 15:03:02 -0300

    feat: dollar currency exchange finished and request to api finished, too.

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+
+export default function RootLayout(){
+    return(
+        <GestureHandlerRootView>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={{title: "American Dollar"}}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
+    )
+}
---
+++ services/awesomeapi.ts
@@ -0,0 +1,43 @@
+let baseUrl = 'https://economia.awesomeapi.com.br/json';
+
+export const getUsd = async() =>{
+    try{
+        let url = baseUrl + '/last/USD-BRL';
+
+        const response = await fetch(url, {
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        const data = await response.json();
+
+        if(data.USDBRL){
+            return parseFloat(data.USDBRL.ask);
+        }
+
+        return 0;
+    }catch(err){
+        return 0;
+   }
+}
+
+export const getEur = async() =>{
+    try{
+        let url = baseUrl+='/last/eur-brl';
+
+        const response = await fetch(url, {
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+
+        if(response.ok){
+            const data = await response.json();
+            return parseFloat(data.EURBRL.ask);
+        }
+
+    return 0;
+    }catch(err){
+        return 0;
+   }
+}
